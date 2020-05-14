350baf2ed6026c688ac99a4a21c1696bcd23f85d
wangtianqi <wangtianqi20@jd.com>
2020-05-14 15:48:36 +0800

    去掉不必要的依赖，仅留DB操作的最少maven依赖

--- src/main/java/com/example/configuration/MyBatisConfig.java
+++ src/main/java/com/example/configuration/MyBatisConfig.java
@@ -1,12 +1,9 @@
 package com.example.configuration;
 
 import com.example.common.MyBatisInterceptor;
-import com.github.pagehelper.PageHelper;
 import org.apache.ibatis.plugin.Interceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Properties;
 
 /**
  * MyBatis Config
@@ -16,23 +13,6 @@ import java.util.Properties;
  */
 @Configuration
 public class MyBatisConfig {
-    /**
-     * 分页对象实列化
-     *
-     * @return
-     */
-    @Bean
-    public PageHelper pageHelper() {
-        PageHelper pageHelper = new PageHelper();
-        Properties p = new Properties();
-        p.setProperty("offsetAsPageNum", "true");
-        p.setProperty("rowBoundsWithCount", "true");
-        p.setProperty("reasonable", "true");
-        p.setProperty("dialect", "mysql");
-        pageHelper.setProperties(p);
-        return pageHelper;
-    }
-
     /**
      * mybatis 自定义拦截器
      */
